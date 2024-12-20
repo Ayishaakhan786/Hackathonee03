b7f237d9f3bda3aaf76dc759a20bc45d23a5a292
Ayishaakhan786 <ayeshakhan228276@gmail.com>
2024-12-20 09:04:55 +0500

    today

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -2,7 +2,9 @@ import Image from "next/image";
 
 import upper from './imgs/upper.png';
 import menu01 from './imgs/M1.png';
-import { PiCoffee } from "react-icons/pi";
+import rightImg from './imgs/right.png';
+import ice from './imgs/ice.png';
+import drinks from './imgs/drinks.png';
 
 export default function Menu () {
     return (
@@ -29,11 +31,185 @@ export default function Menu () {
 {/* second section */}
 <div className="bg-gray-100 p-4 sm:p-6">
   {/* Section container */}
-  <div className="max-w-4xl mx-auto flex flex-col md:flex-row items-center space-y-6 md:space-y-0 md:space-x-6">
+  <div className="max-w-4xl mx-auto flex flex-col-reverse md:flex-row items-center space-y-6 md:space-y-0 md:space-x-6">
     {/* Image */}
     <div className="w-full md:w-1/3">
       <Image
         src={menu01}
+        alt="Starter Menu"
+        className="rounded-lg shadow-md w-full h-auto"
+      />
+    </div>
+    {/* Menu content */}
+    <div className="w-full md:w-2/3">
+      <h2 className="text-xl sm:text-2xl md:text-4xl font-bold text-gray-800 mb-4 sm:mb-6">
+        Starter Menu
+      </h2>
+      <ul className="space-y-6">
+        {/* Menu Item 1 */}
+        <li className="border-b pb-4">
+          <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-2 sm:space-y-0">
+            <h3 className="text-sm sm:text-base md:text-lg font-semibold text-gray-700">
+              Alder Grilled Chinook Salmon
+            </h3>
+            <span className="text-orange-600 font-bold text-sm sm:text-base md:text-lg">
+              32$
+            </span>
+          </div>
+          <p className="text-gray-600 text-xs sm:text-sm md:text-base mt-2">
+            Toasted French bread topped with romano, cheddar
+          </p>
+          <h3 className="text-gray-600 text-xs sm:text-sm md:text-base mt-2">
+            560 CAL
+          </h3>
+        </li>
+        {/* Menu Item 2 */}
+        <li className="border-b pb-4">
+          <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-2 sm:space-y-0">
+            <h3 className="text-sm sm:text-base md:text-lg font-semibold text-amber-500">
+              Berries and creme tart
+            </h3>
+            <span className="text-orange-600 font-bold text-sm sm:text-base md:text-lg">
+              43$
+            </span>
+          </div>
+          <p className="text-gray-600 text-xs sm:text-sm md:text-base mt-2">
+            Gorgonzola, ricotta, mozzarella, taleggio
+          </p>
+          <h3 className="text-gray-600 text-xs sm:text-sm md:text-base mt-2">
+            700 CAL
+          </h3>
+        </li>
+        {/* Menu Item 3 */}
+        <li className="border-b pb-4">
+          <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-2 sm:space-y-0">
+            <h3 className="text-sm sm:text-base md:text-lg font-semibold text-gray-700">
+              Tormentoso Bush Pizza Pintoage
+            </h3>
+            <span className="text-orange-600 font-bold text-sm sm:text-base md:text-lg">
+              14$
+            </span>
+          </div>
+          <p className="text-gray-600 text-xs sm:text-sm md:text-base mt-2">
+            Ground cumin, avocados, peeled and cubed
+          </p>
+          <h3 className="text-gray-600 text-xs sm:text-sm md:text-base mt-2">
+            1000 CAL
+          </h3>
+        </li>
+        {/* Menu Item 4 */}
+        <li>
+          <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-2 sm:space-y-0">
+            <h3 className="text-sm sm:text-base md:text-lg font-semibold text-gray-700">
+              Spicy Vegan Potato Curry
+            </h3>
+            <span className="text-orange-600 font-bold text-sm sm:text-base md:text-lg">
+              35$
+            </span>
+          </div>
+          <p className="text-gray-600 text-xs sm:text-sm md:text-base mt-2">
+            Spreadable cream cheese, crumbled blue cheese
+          </p>
+          <h3 className="text-gray-600 text-xs sm:text-sm md:text-base mt-2">
+            560 CAL
+          </h3>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+{/* Third section */}
+<div className="bg-gray-100 p-4 sm:p-6">
+  {/* Section container */}
+  <div className="max-w-4xl mx-auto flex flex-col md:flex-row items-center space-y-6 md:space-y-0 md:space-x-6">
+    {/* Menu content */}
+    <div className="w-full md:w-2/3">
+      <h2 className="text-2xl sm:text-4xl font-bold text-gray-800 mb-4 sm:mb-6">
+      Main Course
+      </h2>
+      <ul className="space-y-6">
+        {/* Menu Item 1 */}
+        <li className="border-b pb-4">
+          <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-2 sm:space-y-0">
+            <h3 className="text-base sm:text-lg font-semibold text-gray-700">
+            Optic Big Breakfast Combo Menu
+            </h3>
+            <span className="text-orange-600 font-bold text-sm sm:text-base">
+              32$
+            </span>
+          </div>
+          <p className="text-gray-600 text-xs sm:text-sm mt-2">
+          Toasted French bread topped with romano, cheddar
+          </p>
+          <h3 className="text-gray-600 text-xs sm:text-sm mt-2">560 CAL</h3>
+        </li>
+        {/* Menu Item 2 */}
+        <li className="border-b pb-4">
+          <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-2 sm:space-y-0">
+            <h3 className="text-base sm:text-lg font-semibold text-gray-700">
+            Cashew Chicken With Stir-Fry
+            </h3>
+            <span className="text-orange-600 font-bold text-sm sm:text-base">
+              43$
+            </span>
+          </div>
+          <p className="text-gray-600 text-xs sm:text-sm mt-2">
+          Gorgonzola, ricotta, mozzarella, taleggio
+          </p>
+          <h3 className="text-gray-600 text-xs sm:text-sm mt-2">700 CAL</h3>
+        </li>
+        {/* Menu Item 3 */}
+        <li className="border-b pb-4">
+          <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-2 sm:space-y-0">
+            <h3 className="text-base sm:text-lg font-semibold text-gray-700">
+            Vegetables & Green Salad
+            </h3>
+            <span className="text-orange-600 font-bold text-sm sm:text-base">
+              14$
+            </span>
+          </div>
+          <p className="text-gray-600 text-xs sm:text-sm mt-2">
+          Ground cumin, avocados, peeled and cubed
+          </p>
+          <h3 className="text-gray-600 text-xs sm:text-sm mt-2">1000 CAL</h3>
+        </li>
+        {/* Menu Item 4 */}
+        <li>
+          <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-2 sm:space-y-0">
+            <h3 className="text-base sm:text-lg font-semibold text-gray-700">
+            Spicy Vegan Potato Curry
+            </h3>
+            <span className="text-orange-600 font-bold text-sm sm:text-base">
+              35$
+            </span>
+          </div>
+          <p className="text-gray-600 text-xs sm:text-sm mt-2">
+            Spreadable cream cheese, crumbled blue cheese
+          </p>
+          <h3 className="text-gray-600 text-xs sm:text-sm mt-2">560 CAL</h3>
+        </li>
+      </ul>
+    </div>
+    {/* Image */}
+    <div className="w-full md:w-1/3">
+      <Image
+        src={rightImg}
+        alt="Starter Menu"
+        className="rounded-lg shadow-md"
+      />
+    </div>
+  </div>
+</div>
+
+{/* Fourth section */}
+<div className="bg-gray-100 p-4 sm:p-6">
+  {/* Section container */}
+  <div className="max-w-4xl mx-auto flex flex-col md:flex-row items-center space-y-6 md:space-y-0 md:space-x-6">
+    {/* Image */}
+    <div className="w-full md:w-1/3">
+      <Image
+        src={ice}
         alt="Starter Menu"
         className="rounded-lg shadow-md"
       />
@@ -41,14 +217,14 @@ export default function Menu () {
     {/* Menu content */}
     <div className="w-full md:w-2/3">
       <h2 className="text-2xl sm:text-4xl font-bold text-gray-800 mb-4 sm:mb-6">
-        Starter Menu
+      Dessert
       </h2>
       <ul className="space-y-6">
         {/* Menu Item 1 */}
         <li className="border-b pb-4">
           <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-2 sm:space-y-0">
             <h3 className="text-base sm:text-lg font-semibold text-gray-700">
-              Alder Grilled Chinook Salmon
+            Fig and lemon cake
             </h3>
             <span className="text-orange-600 font-bold text-sm sm:text-base">
               32$
@@ -62,8 +238,8 @@ export default function Menu () {
         {/* Menu Item 2 */}
         <li className="border-b pb-4">
           <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-2 sm:space-y-0">
-            <h3 className="text-base sm:text-lg font-semibold text-amber-500">
-              Berries and creme tart
+            <h3 className="text-base sm:text-lg font-semibold text-gray-700">
+            Creamy mascarpone cake
             </h3>
             <span className="text-orange-600 font-bold text-sm sm:text-base">
               43$
@@ -78,7 +254,7 @@ export default function Menu () {
         <li className="border-b pb-4">
           <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-2 sm:space-y-0">
             <h3 className="text-base sm:text-lg font-semibold text-gray-700">
-              Tormentoso Bush Pizza Pintoage
+            Pastry, blueberries, lemon juice
             </h3>
             <span className="text-orange-600 font-bold text-sm sm:text-base">
               14$
@@ -93,7 +269,7 @@ export default function Menu () {
         <li>
           <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-2 sm:space-y-0">
             <h3 className="text-base sm:text-lg font-semibold text-gray-700">
-              Spicy Vegan Potato Curry
+            Pain au chocolat
             </h3>
             <span className="text-orange-600 font-bold text-sm sm:text-base">
               35$
@@ -105,6 +281,97 @@ export default function Menu () {
           <h3 className="text-gray-600 text-xs sm:text-sm mt-2">560 CAL</h3>
         </li>
       </ul>
+    </div>
+  </div>
+</div>
+
+{/* fifth section */}
+<div className="bg-gray-100 p-4 sm:p-6">
+  {/* Section container */}
+  <div className="max-w-4xl mx-auto flex flex-col-reverse md:flex-row items-center space-y-6 md:space-y-0 md:space-x-6">
+    {/* Menu content */}
+    <div className="w-full md:w-2/3">
+      <h2 className="text-xl sm:text-2xl md:text-4xl font-bold text-gray-800 mb-4 sm:mb-6">
+        Drinks
+      </h2>
+      <ul className="space-y-6">
+        {/* Menu Item 1 */}
+        <li className="border-b pb-4">
+          <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-2 sm:space-y-0">
+            <h3 className="text-sm sm:text-base md:text-lg font-semibold text-gray-700">
+              Caffè macchiato
+            </h3>
+            <span className="text-orange-600 font-bold text-sm sm:text-base md:text-lg">
+              32$
+            </span>
+          </div>
+          <p className="text-gray-600 text-xs sm:text-sm md:text-base mt-2">
+            Toasted French bread topped with romano, cheddar
+          </p>
+          <h3 className="text-gray-600 text-xs sm:text-sm md:text-base mt-2">
+            560 CAL
+          </h3>
+        </li>
+        {/* Menu Item 2 */}
+        <li className="border-b pb-4">
+          <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-2 sm:space-y-0">
+            <h3 className="text-sm sm:text-base md:text-lg font-semibold text-gray-700">
+              Aperol Spritz Capacianno
+            </h3>
+            <span className="text-orange-600 font-bold text-sm sm:text-base md:text-lg">
+              43$
+            </span>
+          </div>
+          <p className="text-gray-600 text-xs sm:text-sm md:text-base mt-2">
+            Gorgonzola, ricotta, mozzarella, taleggio
+          </p>
+          <h3 className="text-gray-600 text-xs sm:text-sm md:text-base mt-2">
+            700 CAL
+          </h3>
+        </li>
+        {/* Menu Item 3 */}
+        <li className="border-b pb-4">
+          <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-2 sm:space-y-0">
+            <h3 className="text-sm sm:text-base md:text-lg font-semibold text-gray-700">
+              Caffe Latte Campuri
+            </h3>
+            <span className="text-orange-600 font-bold text-sm sm:text-base md:text-lg">
+              14$
+            </span>
+          </div>
+          <p className="text-gray-600 text-xs sm:text-sm md:text-base mt-2">
+            Ground cumin, avocados, peeled and cubed
+          </p>
+          <h3 className="text-gray-600 text-xs sm:text-sm md:text-base mt-2">
+            1000 CAL
+          </h3>
+        </li>
+        {/* Menu Item 4 */}
+        <li>
+          <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-2 sm:space-y-0">
+            <h3 className="text-sm sm:text-base md:text-lg font-semibold text-gray-700">
+              Tormentoso BushTea Pintoage
+            </h3>
+            <span className="text-orange-600 font-bold text-sm sm:text-base md:text-lg">
+              35$
+            </span>
+          </div>
+          <p className="text-gray-600 text-xs sm:text-sm md:text-base mt-2">
+            Spreadable cream cheese, crumbled blue cheese
+          </p>
+          <h3 className="text-gray-600 text-xs sm:text-sm md:text-base mt-2">
+            560 CAL
+          </h3>
+        </li>
+      </ul>
+    </div>
+    {/* Image */}
+    <div className="w-full md:w-1/3">
+      <Image
+        src={drinks}
+        alt="Starter Menu"
+        className="rounded-lg shadow-md w-full h-auto"
+      />
     </div>
   </div>
 </div>
